cbb48b8bc93dc9449bbe9c38d90b55db57d036d4
Andreas Siegel <andreas.siegel@smartrac-group.com>
2018-03-20 14:22:15 +0100

    Section 30: What makes up an HTTP Request

--- asynchronous/weather-app/app.js
+++ asynchronous/weather-app/app.js
@@ -6,5 +6,9 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${apiKey.API_KEY}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    var result = body.results[0];
+    var geolocation = result.geometry.location;
+    console.log(`Address: ${result.formatted_address}`);
+    console.log(`Latitude: ${geolocation.lat}`);
+    console.log(`Longitude: ${geolocation.lng}`);
 });
