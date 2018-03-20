8068430baaf2e4818a9f4b5710cd9f77a07726dc
Andreas Siegel <andreas.siegel@smartrac-group.com>
2018-03-20 14:02:41 +0100

    Section 28: Callback Functions & APIs

---
+++ asynchronous/weather-app/app.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+const apiKey = require('./api-key');
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${apiKey.API_KEY}`,
+    json: true
+}, (error, response, body) => {
+    console.log(body);
+});
