85bcd1c675dc256cf271641444505dfd48bcaa9f
Andreas Siegel <andreas.siegel@smartrac-group.com>
2018-03-22 16:22:25 +0100

    Section 32: Callback Errors

--- asynchronous/weather-app/app.js
+++ asynchronous/weather-app/app.js
@@ -22,9 +22,15 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey.API_KEY}`,
     json: true
 }, (error, response, body) => {
-    var result = body.results[0];
-    var geolocation = result.geometry.location;
-    console.log(`Address: ${result.formatted_address}`);
-    console.log(`Latitude: ${geolocation.lat}`);
-    console.log(`Longitude: ${geolocation.lng}`);
+    if (error) {
+        console.log('Unable to connect to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+        var result = body.results[0];
+        var geolocation = result.geometry.location;
+        console.log(`Address: ${result.formatted_address}`);
+        console.log(`Latitude: ${geolocation.lat}`);
+        console.log(`Longitude: ${geolocation.lng}`);
+    }
 });
